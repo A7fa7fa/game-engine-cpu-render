491f88816970ef177ae1792c30ed136861e328af
Manuel Seidler <Manuel.Seidler@de.bosch.com>
2023-11-21 17:56:28 +0100

    add audio support and character

--- src/main/java/org/a7fa7fa/game/GameManager.java
+++ src/main/java/org/a7fa7fa/game/GameManager.java
@@ -4,6 +4,7 @@ import org.a7fa7fa.engine.AbstractGame;
 import org.a7fa7fa.engine.Color;
 import org.a7fa7fa.engine.GameContainer;
 import org.a7fa7fa.engine.Renderer;
+import org.a7fa7fa.engine.audio.SoundClip;
 import org.a7fa7fa.engine.gfx.Image;
 import org.a7fa7fa.engine.gfx.ImageTile;
 
@@ -15,18 +16,55 @@ public class GameManager extends AbstractGame {
     private Image cursor;
     private ImageTile backgroundTile;
     private ImageTile sprite;
+    private ImageTile character;
+    private SoundClip clip;
 
     public GameManager() {
         backgroundTile = new ImageTile("/grasstile_16_16_7.png", 16, 16);
         cursor = new Image("/cursor.png");
         sprite = new ImageTile("/weed.png", 16, 16);
+        character = new ImageTile("/char_16_16_4.png", 16, 16);
+        clip = new SoundClip("/audio/clap.wav");
+        clip.setVolume(-20);
     }
+
+    int charX = -1;
+    int charY = 0;
+    int charTile = 0;
+
 
     @Override
     public void update(GameContainer gameContainer, float deltaTime) {
-        if (gameContainer.getInput().isKeyDown(KeyEvent.VK_A)) {
-            System.out.println("Key A was pressed");
+
+        if (charX == -1) {
+            charX = gameContainer.getWidth() / 2;
+            charY = gameContainer.getHeight() / 2;
         }
+
+        if (gameContainer.getInput().isKeyDown(KeyEvent.VK_SPACE)) {
+            clip.play();
+        }
+
+        if (gameContainer.getInput().isKey(KeyEvent.VK_A)) {
+            charTile = 2;
+            charX -= 1;
+        }
+
+        if (gameContainer.getInput().isKey(KeyEvent.VK_D)) {
+            charTile = 3;
+            charX += 1;
+        }
+        if (gameContainer.getInput().isKey(KeyEvent.VK_W)) {
+            charTile = 1;
+            charY -= 1;
+        }
+        if (gameContainer.getInput().isKey(KeyEvent.VK_S)) {
+            charTile = 0;
+            charY += 1;
+        }
+
+
+
         spritePos += deltaTime * 12;
         if (spritePos >= 9){
             spritePos = 0;
@@ -64,6 +102,8 @@ public class GameManager extends AbstractGame {
         renderer.drawImageTile(sprite, 50, 50, (int)spritePos, 0);
 
         renderer.drawImageTile(sprite, 100, 60, (int)spritePos, 0);
+
+        renderer.drawImageTile(character, charX, charY, charTile, 0);
 
         renderer.drawImage(cursor, gameContainer.getInput().getMouseX() - 8, gameContainer.getInput().getMouseY() - 8);
         renderer.drawText("Fps: " + String.valueOf(gameContainer.getFps()), 1,1, Color.WHITE.getHexValue());
